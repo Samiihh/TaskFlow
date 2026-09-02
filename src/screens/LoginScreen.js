f1c25173fc28290e822bd0e6d71cc6686a48019e
Samiihh <saahhlulu2@gmail.com>
2026-09-02 19:52:58 -0300

    Atualizando projeto

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, View } from "react-native";
+import Input from "../components/Input";
 
 export default function LoginScreen({ navigation, onLogin }) {
 
@@ -60,26 +61,64 @@ export default function LoginScreen({ navigation, onLogin }) {
                 // permite que tenhamos interações com os campos mesmo com o teclado aberto.
                 keyboardShouldPersistTaps="handled"
             >
-            
-            <View styles={styles.brand}>
 
-                {/* Logo */}
-                <View style={styles.logo}>
-                    <Text style={styles.logoText}>{'\u2713'}</Text>
+                <View styles={styles.brand}>
+
+                    {/* Logo */}
+                    <View style={styles.logo}>
+                        <Text style={styles.logoText}>{'\u2713'}</Text>
+                    </View>
+
+
+                    {/* Titulo */}
+                    <Text style={styles.title}>
+                        Bem-Vindo ao TaskFlow
+                    </Text>
+
+                    {/* subtitulo */}
+
+                    <Text styles={styles.subtitle}>
+                        Entre para organizar suas tarefas do dia.
+                    </Text>
+
+                    {/* formulario */}
+
+                    <View style={styles.form}>
+                        {/* campos de input do formulario */}
+                        <Input
+                            label="Seu nome"
+                            value={name}
+                            onChangeText={setName}
+                            placeHolder="Digit seu nome"
+                        />
+                        <Input
+                            label="E-mail"
+                            value={email}
+                            onChangeText={setEmail}
+                            placeHolder="Digite seu e-mail"
+                        />
+                        <Input
+                            label="Senha"
+                            value={passWord}
+                            onChangeText={setPassWord}
+                            placeHolder="Digite sua senha"
+                        />
+
+
+                        {/* mensagem de erro */}
+
+                        {error ? (
+                            <Text style={styles.error}>
+                                {error}
+                            </Text>
+                        ) : null}
+                    </View>
+
+
+
+
+
                 </View>
-
-
-                {/* Titulo */}
-                <Text style={styles.title}>
-                    Bem-Vindo ao TaskFlow
-                </Text>
-
-            
-
-
-
-
-            </View>
 
 
             </ScrollView>
@@ -88,5 +127,5 @@ export default function LoginScreen({ navigation, onLogin }) {
 }
 
 const styles = StyleSheet.create({
-    
+
 })
